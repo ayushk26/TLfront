923b44df794323d96de5b15de95c93c09062b858
Ayush Kapoor <ayushk681@gmail.com>
2021-08-16 03:25:22 +0530

    about page updated

--- src/assets/js/new/aosnew.js
+++ src/assets/js/new/aosnew.js
@@ -9,4 +9,18 @@ VANTA.WAVES({
     scale: 1.0,
     scaleMobile: 1.0,
     color: 0x6052a,
-})},100)
+})
+VANTA.TOPOLOGY({
+    el: ".aboutBody",
+    mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.0,
+        minWidth: 200.0,
+        scale: 1.0,
+        scaleMobile: 1.0,
+        color: 0x3184a,
+        backgroundColor: 0x6052a,
+  })
+
+},100)
